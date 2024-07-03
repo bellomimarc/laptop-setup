51867446fe90a3db59e1d63b74d55956b5f9f7ab
Marcello Bellomi <marcello.bellomi@gmail.com>
2024-07-03 09:28:11 +0200

    add slack

--- setup.sh
+++ setup.sh
@@ -59,6 +59,10 @@ else
     echo -e "${GREEN}VSCode already installed!${NC}"
 fi
 
+# install slack
+sudo snap install slack
+echo -e "${GREEN}Slack installed!${NC}"
+
 # install pgadmin
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'

8bb396dd5ad94c193837749de3e0bb8102caa17d
Marcello Bellomi <marcello.bellomi@gmail.com>
2024-07-04 09:51:57 +0200

    add aws cli

--- setup-qomodo.sh
+++ setup-qomodo.sh
@@ -9,3 +9,11 @@ NC='\033[0m' # No Color
 # install slack
 sudo snap install slack
 echo -e "${GREEN}Slack installed!${NC}"
+
+# install aws cli
+sudo apt install -y curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+echo -e "${GREEN}AWS CLI installed!${NC}"
